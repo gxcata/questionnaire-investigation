26c620b1cf75d73e50c060dc7ae6b5dafdc2d81e
gxcata <gxcata@163.com>
2023-01-28 10:07:30 +0800

    优化

--- src/main/java/com/gxcata/questionnaireinvestigation/po/AddCompanyPO.java
+++ src/main/java/com/gxcata/questionnaireinvestigation/po/AddCompanyPO.java
@@ -3,6 +3,7 @@ package com.gxcata.questionnaireinvestigation.po;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.Date;
 
 /**
@@ -15,11 +16,13 @@ public class AddCompanyPO {
     private String companyId;
 
     @ApiModelProperty(value = "单位名称",required = true)
+    @NotEmpty(message = "单位名称不能为空")
     private String companyName;
 
     @ApiModelProperty(value = "单位性质（政府部门：10000101；国资企业：10000102；科研机构：10000103；" +
             "医疗机构：10000104；金融机构：10000105；权力机构：10000106；" +
             "民营企业：10000107；其他客户：10000108）",required = true)
+    @NotEmpty(message = "单位性质不能为空")
     private String companyNatureCode;
 
     @ApiModelProperty(value = "创建时间",hidden = true)
